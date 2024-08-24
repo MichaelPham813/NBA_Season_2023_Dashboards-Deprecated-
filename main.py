47e8a84063929a51ef5b68dee526e97af62085bf
Michael <pth.a10.08.20132014@gmail.com>
2024-08-24 13:23:11 -0400

    Add environment and gitignore

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 from flask import Flask,render_template,request
 from plot_graph import player_weight_hist,percentage_player_weight_hist,player_stats_bar,all_stats_bar,player_stats_avg
 
@@ -12,4 +13,20 @@ def index():
     return render_template("index.html")
 
 if __name__ == '__main__':
+=======
+from flask import Flask,render_template,request
+from plot_graph import player_weight_hist,percentage_player_weight_hist,player_stats_bar,all_stats_bar,player_stats_avg
+
+app = Flask(__name__)
+@app.route('/')
+def index():
+    player_weight_hist()
+    percentage_player_weight_hist()
+    player_stats_bar()
+    all_stats_bar()
+    player_stats_avg()
+    return render_template("index.html")
+
+if __name__ == '__main__':
+>>>>>>> 8325578 (Add .gitignore and environment file)
   app.run(host='0.0.0.0', port=5000)
